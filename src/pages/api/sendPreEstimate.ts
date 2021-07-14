ba436eae8087a7ce3c823637cd8ba5f760bc3ab0
Marcos <loseonjr@gmail.com>
2021-07-14 12:08:59 -0300

    config update

--- src/pages/api/sendPreEstimate.ts
+++ src/pages/api/sendPreEstimate.ts
@@ -6,15 +6,15 @@ mail.setApiKey(process.env.SEND_API_KEY)
 
 export default (request: NowRequest, response: NowResponse) => {
    
-   response.setHeader('Access-Control-Allow-Credentials', <any>true)
-   response.setHeader('Access-Control-Allow-Origin', '*')
-   // another common pattern
-   // response.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-   response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-   response.setHeader(
-     'Access-Control-Allow-Headers',
-     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-   )
+   // response.setHeader('Access-Control-Allow-Credentials', <any>true)
+   // response.setHeader('Access-Control-Allow-Origin', '*')
+   // // another common pattern
+   // // response.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+   // response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+   // response.setHeader(
+   //   'Access-Control-Allow-Headers',
+   //   'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+   // )
  
    const {name,phone,email, device, model, description  } = request.body.email;
 
